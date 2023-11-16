50a816a6e730ceba8e83d1af824eae7dc3b1c488
Harsh19704 <harsh.r1@ahduni.edu.in>
2023-11-16 21:18:21 +0530

    lab 12

---
+++ assignment/08.11.2023/700.java
@@ -0,0 +1,15 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+       if (root == null){
+           return null;
+       } 
+       else if (root.val == val){
+           TreeNode res = root;
+           return res;
+       }
+       else if(root.val > val){
+           return searchBST(root.left, val);
+       }
+       return searchBST(root.right, val);
+    }
+}
